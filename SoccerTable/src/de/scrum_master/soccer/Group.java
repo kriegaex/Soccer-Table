86241c10632f5c8bde51677bdb594a2739530c4e
Alexander Kriegisch <Alexander@Kriegisch.name>
2012-07-29 13:39:17 +0200

    Add Group.parseCrossTable and Primera División test cases
    The new method is capable of parsing comma-separated cross-tables (see
    JavaDoc for details).
    
    The new test cases from the Spanish first league comprise all results
    from seasons 2007/08 to 2011/12. BTW, even though I downloaded the
    results from http://fussballdaten.de, the resulting tables there
    sometimes show erroneous ranking for teams with equal points. Obviously
    sub-table calculation for direct comparison is somehow wrong. I notified
    the web master and hope he will fix it.

--- SoccerTable/src/de/scrum_master/soccer/Group.java
+++ SoccerTable/src/de/scrum_master/soccer/Group.java
@@ -1,6 +1,10 @@
 package de.scrum_master.soccer;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Scanner;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -17,6 +21,66 @@ class Group implements Comparable<Group>
 		this.id = id;
 		this.name = name;
 		table = new Table();
+	}
+
+	/**
+	 * Create a Group from raw text data representing a cross-table of each team's match results against
+	 * each other team, i.e. the usual two matches per pair, one home and one guest match.
+	 *
+	 * @param id unique group/league ID
+	 * @param name full group/league name
+	 * @param rawData cross-table raw data (comma-separated) with one row/line per team. The first
+	 * table column is the team name, the remaining ones are the home match results, e.g.
+	 * <pre>
+	 * Team A,?,3,0,2,1
+	 * Team B,1,2,?,2,2
+	 * Team C,0,1,2,3,?
+	 * </pre>
+	 * Each "?" is a dummy value because no team plays against itself. In the above example Team A
+	 * beat B at home by 3-0 and as guest team by 1-2. Team C lost against A at home by 0-1 and also
+	 * 2-1 as guest team. Team B tied C at home by 2-2.
+	 * <p>
+	 * <b>Please note:</b> The simplified cross-table reformat does <i>not</i> contain any match dates,
+	 * so this method creates synthetic, identical ones without any real-world significance.
+	 * @return the Group generated from the cross-table
+	 */
+	static Group parseCrossTable(String id, String name, String rawData)
+	{
+		Group group = new Group(id, name);
+
+		// Determine teams
+		List<Team> teams = new ArrayList<Team>();
+		Scanner scanner = new Scanner(rawData);
+		scanner.useDelimiter("[,\n]");
+		while (scanner.hasNext()) {
+			String teamName = scanner.next();
+			group.addTeam(new Team(teamName, teamName));
+			teams.add(group.getTeam(teamName));
+			scanner.nextLine();
+		}
+		scanner.close();
+
+		// Parse match results
+		scanner = new Scanner(rawData);
+		scanner.useDelimiter("[,\n]");
+		Calendar dummyMatchDate = Calendar.getInstance();
+		for (Team homeTeam : teams) {
+			scanner.next();
+			for (Team guestTeam : teams) {
+				if (homeTeam.equals(guestTeam)) {
+					scanner.next();
+					continue;
+				}
+				group.addMatch(new Match(
+					dummyMatchDate,
+					homeTeam, guestTeam,
+					scanner.nextInt(), scanner.nextInt()
+				));
+			}
+			scanner.nextLine();
+		}
+		scanner.close();
+		return group;
 	}
 
 	public int compareTo(Group group)
