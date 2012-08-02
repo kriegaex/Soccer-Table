96d3ce6659850c8b0da58377569e88a4d7872531
Alexander Kriegisch <Alexander@Kriegisch.name>
2012-08-02 23:50:24 +0200

    Ranking: reformat for fewer LOC, making strategies more readable

--- SoccerTable/src/de/scrum_master/soccer/ranking/Ranking.java
+++ SoccerTable/src/de/scrum_master/soccer/ranking/Ranking.java
@@ -1,71 +1,44 @@
 package de.scrum_master.soccer.ranking;
 
 public enum Ranking {
+	// Distinct ranking strategies
 	DIRECT_COMPARISON(
 		new PointsComparator(
 			new PointsComparator(
-				null,
-				new GoalsDifferenceComparator(
-					null,
-					new GoalsForComparator(null, null)
-				)
-			),
+				null, new GoalsDifferenceComparator(
+					null, new GoalsForComparator(null, null))),
 			new GoalsDifferenceComparator(
-				null,
-				new GoalsForComparator(null, null)
-			)
-		)
+				null, new GoalsForComparator(null, null)))
 	),
 	DIRECT_COMPARISON_WITH_GOALS_AWAY(
 		new PointsComparator(
 			new PointsComparator(
-				null,
-				new GoalsDifferenceComparator(
-					null,
-					new GoalsAwayComparator(null, null)
-				)
-			),
+				null, new GoalsDifferenceComparator(
+					null, new GoalsAwayComparator(null, null))),
 			new GoalsDifferenceComparator(
-				null,
-				new GoalsForComparator(null, null)
-			)
-		)
+				null, new GoalsForComparator(null, null)))
 	),
 	GOALS_DIFFERENCE(
 		new PointsComparator(
-			null,
-			new GoalsDifferenceComparator(
-				null,
-				new GoalsForComparator(
+			null, new GoalsDifferenceComparator(
+				null, new GoalsForComparator(
 					new PointsComparator(
-						null,
-						new GoalsDifferenceComparator(
-							null,
-							new GoalsForComparator(
-								null,
-								new GoalsAwayComparator(null, null)
-							)
-						)
-					),
-					new GoalsAwayComparator(null, null)
-				)
-			)
-		)
+						null, new GoalsDifferenceComparator(
+							null, new GoalsForComparator(
+								null, new GoalsAwayComparator(null, null)))),
+					new GoalsAwayComparator(null, null))))
 	),
 	GOALS_AWAY(
 		new PointsComparator(
-			null,
-			new GoalsDifferenceComparator(
-				null,
-				new GoalsAwayComparator(null, null)
-			)
-		)
+			null, new GoalsDifferenceComparator(
+				null, new GoalsAwayComparator(null, null)))
 	),
-	EURO_2012(DIRECT_COMPARISON.comparator),
-	PRIMERA_DIVISION(DIRECT_COMPARISON.comparator),
-	BUNDESLIGA(GOALS_DIFFERENCE.comparator),
-	CHAMPIONS_LEAGUE_GROUP(DIRECT_COMPARISON_WITH_GOALS_AWAY.comparator),
-	CHAMPIONS_LEAGUE_KO(GOALS_AWAY.comparator);
+	// Alias names
+	EURO_2012              (DIRECT_COMPARISON.comparator),
+	PRIMERA_DIVISION       (DIRECT_COMPARISON.comparator),
+	BUNDESLIGA             (GOALS_DIFFERENCE.comparator),
+	CHAMPIONS_LEAGUE_GROUP (DIRECT_COMPARISON_WITH_GOALS_AWAY.comparator),
+	CHAMPIONS_LEAGUE_KO    (GOALS_AWAY.comparator);
 
 	public final TableRowComparator comparator;
 
